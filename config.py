2113ec7113771ab6e5f52d1d646ce1e2197d73ea
Dan Lloyd <spoonster@gmail.com>
2014-02-09 22:12:38 -0500

    moar postgres plz

--- config.py
+++ config.py
@@ -1,7 +1,8 @@
 import os
 basedir = os.path.abspath(os.path.dirname(__file__))
 
-SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+#SQLALCHEMY_DATABASE_URI = 'sqlite:///' + os.path.join(basedir, 'app.db')
+SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://wines:ghsdlknga34sd@localhost/wines'
 SQLALCHEMY_MIGRATE_REPO = os.path.join(basedir, 'db_repository')
 BOOTSTRAP_SERVE_LOCAL = True
 
